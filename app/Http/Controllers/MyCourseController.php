9d34af3ca656fcf2b539aab9b1c221f632b83c5b
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2020-12-15 21:01:47 +0700

    update MyCourse, helpers, api

--- app/Http/Controllers/MyCourseController.php
+++ app/Http/Controllers/MyCourseController.php
@@ -72,10 +72,46 @@ class MyCourseController extends Controller
             ], 409);
         }
 
-        $myCourse  = MyCourse::create($data);
+        if($course->type === 'premium'){
+            if($course->price === 0){
+                return response()->json([
+                    'status' =>'error',
+                    'message' => 'Price can\'t be 0'
+                ],405);
+            }
+
+            $order = postOrder([
+                'user' => $user['data'],
+                'course' => $course->toArray()
+            ]);
+
+            if($order['status'] === 'error'){
+                return response()->json([
+                    'status' => $order['status'],
+                    'message' => $order['message']
+                ], $order['http_code']);
+            }
+
+            return response()->json([
+                'status' => $order['status'],
+                'data' => $order['data']
+            ]);
+        } else {
+            $myCourse  = MyCourse::create($data);
+
+            return response()->json([
+                'status' => 'succes',
+                'data' => $myCourse
+            ]);
+        }
+    }
+
+    public function createPremiumAccess(Request $request){
+        $data = $request->all();
+        $myCourse = MyCourse::create($data);
 
         return response()->json([
-            'status' => 'succes',
+            'status' => 'success',
             'data' => $myCourse
         ]);
     }
